f2faf2fd5c04f4c7b90df30939c1497b28af6191
Dylan <33075764+mdylantk@users.noreply.github.com>
2026-01-10 19:36:34 -0500

    update
    Since I wan using the default ignore for godot (plus extra), it was ignoring Data_Tables because mono and 'data_' prefixes. I rename it Tables for now and try not to name folders with data.

---
+++ Game/Tables/Fish_Table.gd
@@ -0,0 +1,44 @@
+#@tool
+class_name Fish_Table extends Random_Table_Resource
+#this is a test. Rare fish should be location base or distance base
+#so another system would need to pick between two table. the rare one
+#with a list limited to a certain area
+
+func _init():
+	if table.is_empty():
+		table.append(Weighted_Group_Resource.new("common",75,
+		[
+			"Carp",
+			"Guppy",
+			"Perch",
+			"Barb"
+		]
+		))
+		table.append(Weighted_Group_Resource.new("uncommon",75,
+		[
+			"Koi",
+			"Bass",
+			"Eel",
+			"Pike",
+			"Salmon",
+			"Walleye",
+			"Trout"
+		]
+		))
+		table.append(Weighted_Group_Resource.new("rare",100,
+		[
+			"Rainbow Trout",
+			"Moon Koi",
+			"Black Bass",
+			"Silver Guppy",
+			"Shadow Pike",
+			"Orange Eel",
+			"Fade Barb",
+			"King Walleye",
+			"Killer Salmon",
+			"Triple Perch",
+			"Apple Carp",
+			"Snake Eel", 
+			"Doom Pike"
+		]
+		))
